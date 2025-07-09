60645ab2b1ef8d420f3217aa6eedae33f9df7160
Dreamboy-26 <heyimpankaj2616@gmail.com>
2025-07-09 21:06:16 +0530

    url changed

--- frontend/src/util/axios.js
+++ frontend/src/util/axios.js
@@ -3,8 +3,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  // baseURL: 'https://refrll-backend.onrender.com/api',
-   baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://refrll-backend.onrender.com/api',
+  //  baseURL: 'http://localhost:5000/api',
   withCredentials: true,
 });
 
