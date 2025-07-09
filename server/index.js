94649f4ed6e1f50a8301c44006d54a8e2e849e24
Dreamboy-26 <heyimpankaj2616@gmail.com>
2025-07-09 14:21:08 +0530

    done

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use(helmet.hsts({
 // Enable CORS for frontend
 app.use(
   cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://refrll-frontend.onrender.com', 
     credentials: true,
 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
